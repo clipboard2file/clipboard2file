44ad89096048ea153f65dbf5e9252e31fda3a31e
Vord <32182314+vord1080@users.noreply.github.com>
2021-10-01 05:29:08 -0500

    Update content.js

--- content_script/content.js
+++ content_script/content.js
@@ -1,88 +1,70 @@
 let clientX;
 let clientY;
-let currentAside;
 
-(async () => {
-  async function handleClick(e) {
-    if (e.target.matches("input[type=file]:not([webkitdirectory], [directory])")) {
-      e.preventDefault();
+async function handleClick(e) {
+  if (e.target.matches("input[type=file]:not([webkitdirectory], [directory])")) {
+    e.preventDefault();
 
-      const clipboardItems = await navigator.clipboard.read();
+    const clipboardItems = await navigator.clipboard.read();
 
-      const clipboardImageItem = clipboardItems.find((item) => item.types.includes("image/png"));
+    const clipboardImageItem = clipboardItems.find((item) => item.types.includes("image/png"));
 
-      if (clipboardImageItem) {
-        if (currentAside?.remove) currentAside.remove();
+    if (clipboardImageItem) {
+      const aside = document.createElement("aside");
+      aside.style.all = "unset";
+      aside.setAttribute("tabindex", -1);
+      const shadow = aside.attachShadow({ mode: "closed", delegatesFocus: true });
+      const theImage = await clipboardImageItem.getType("image/png");
+      const frameRequest = await fetch(browser.runtime.getURL(`content_script/frame.html`));
 
-        const aside = document.createElement("aside");
-        const shadow = aside.attachShadow({ mode: "closed", delegatesFocus: true });
-        const theImage = await clipboardImageItem.getType("image/png");
-        const frameRequest = await fetch(browser.runtime.getURL(`content_script/frame.html`));
+      shadow.innerHTML = await frameRequest.text();
 
-        shadow.innerHTML = await frameRequest.text();
+      const preview = shadow.getElementById("preview");
+      preview.src = URL.createObjectURL(theImage);
+      const root = shadow.getElementById("root");
+      const selectAll = shadow.getElementById("selectAll");
 
-        const preview = shadow.getElementById("preview");
-        const root = shadow.getElementById("root");
-        const selectAll = shadow.getElementById("selectAll");
+      root.style.left = (clientX < 0 ? 0 : clientX + window.visualViewport.pageLeft) + "px";
+      root.style.top = (clientY - 200 < 0 ? clientY + window.visualViewport.pageTop : clientY + window.visualViewport.pageTop - 200) + "px";
 
-        preview.src = URL.createObjectURL(theImage);
-        root.style.left = (clientX < 0 ? 0 : clientX + document.documentElement.scrollLeft) + "px";
-        root.style.top = (clientY - 200 < 0 ? clientY + document.documentElement.scrollTop : clientY + document.documentElement.scrollTop - 200) + "px";
+      preview.addEventListener(
+        "click",
+        () => {
+          aside.remove();
+          const dataTransfer = new DataTransfer();
 
-        preview.addEventListener(
-          "click",
-          () => {
-            aside.remove();
-            const dataTransfer = new DataTransfer();
-            dataTransfer.items.add(new File([theImage], `${window.performance.now()}.png`));
-            e.target.files = dataTransfer.files;
-            e.target.dispatchEvent(new Event("change", { bubbles: true }));
-          },
-          { once: true }
-        );
-        selectAll.addEventListener(
-          "click",
-          () => {
-            aside.remove();
-            replaceFilesOnInputWithFilesFromFakeInputAndYeah(e);
-          },
-          { once: true }
-        );
+          const date = new Date(Date.now());
+          const currentDateTime = new Date(date.getTime() - date.getTimezoneOffset() * 60 * 1000).toISOString();
+          const filenameDate = currentDateTime.substring(0, 10);
+          const filenameTime = currentDateTime.substring(11, 19).replace(/:/g, "-");
 
-        document.documentElement.append(aside);
-        currentAside = aside;
-      } else {
-        replaceFilesOnInputWithFilesFromFakeInputAndYeah(e);
-      }
+          dataTransfer.items.add(new File([theImage], `img-${filenameDate}-${filenameTime}.png`));
+          e.target.files = dataTransfer.files;
+          e.target.dispatchEvent(new Event("change", { bubbles: true }));
+        },
+        { once: true }
+      );
+      selectAll.addEventListener(
+        "click",
+        () => {
+          aside.remove();
+          replaceFilesOnInputWithFilesFromFakeInputAndYeah(e);
+        },
+        { once: true }
+      );
+
+      aside.addEventListener("focusout", (e) => aside.remove(), { passive: true, once: true });
+
+      document.documentElement.append(aside);
+      aside.focus({ preventScroll: true });
+    } else {
+      replaceFilesOnInputWithFilesFromFakeInputAndYeah(e);
     }
   }
+}
 
-  document.addEventListener("click", handleClick);
-  document.addEventListener(
-    "pointerdown",
-    (e) => {
-      clientX = e.clientX;
-      clientY = e.clientY;
-
-      if (e.target.isEqualNode(currentAside) === false && currentAside?.remove) currentAside.remove();
-    },
-    { passive: true }
-  );
-  window.addEventListener(
-    "resize",
-    (e) => {
-      if (currentAside?.remove) currentAside.remove();
-    },
-    { passive: true }
-  );
-  window.addEventListener(
-    "blur",
-    (e) => {
-      if (currentAside?.remove) currentAside.remove();
-    },
-    { passive: true }
-  );
-})();
+document.addEventListener("click", handleClick);
+document.addEventListener("pointerdown", (e) => ((clientX = e.clientX), (clientY = e.clientY)), { passive: true });
 
 function replaceFilesOnInputWithFilesFromFakeInputAndYeah(e) {
   const newInput = document.createElement("input");
