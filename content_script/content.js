e90f705db00be366e7a27099dec68c51bbcd93ef
Clipboard2File <clipboard2file@proton.me>
2022-08-12 23:02:45 -0500

    Refactor extension to use iframes.

--- content_script/content.js
+++ content_script/content.js
@@ -13,128 +13,125 @@ async function handleClick(e) {
     // At some point we should handle multiple images being in the clipboard, which is possible apparently
     const clipboardImageItem = clipboardItems.find((item) => item.types.includes("image/png"));
 
-    if (clipboardImageItem) {
-      const aside = document.createElement("aside");
-      const shadow = aside.attachShadow({ mode: "closed", delegatesFocus: true });
-      const clipboardImage = await clipboardImageItem.getType("image/png");
-      const settings = await browser.storage.local.get(["showFilenameBox", "clearOnPaste", "defaultFilename"]);
-      const frameRequest = await fetch(browser.runtime.getURL(`content_script/frame.html`));
-      const styleRequest = await fetch(browser.runtime.getURL(`content_script/frame.css`));
-      const frameFragment = document.createRange().createContextualFragment(await frameRequest.text());
+    if (!clipboardImageItem) return e.target.showPicker();
 
-      const styleElem = document.createElement("style");
-      styleElem.textContent = await styleRequest.text();
-      shadow.appendChild(styleElem);
-      shadow.append(frameFragment);
-      const root = shadow.getElementById("root");
-      const preview = shadow.getElementById("preview");
-      const selectAll = shadow.getElementById("selectAll");
-      const filenameInput = shadow.getElementById("filename");
+    const aside = document.createElement("aside");
+    const iframe = document.createElement("iframe");
+    const shadow = aside.attachShadow({ mode: "closed" });
+    const clipboardImage = await clipboardImageItem.getType("image/png");
+    const shadowStyleRequest = await fetch(browser.runtime.getURL(`content_script/frame.css`));
+    const iframeRequest = await fetch(browser.runtime.getURL(`content_script/iframe.html`));
+    const iframeStyleRequest = await fetch(browser.runtime.getURL(`content_script/iframe.css`));
 
-      let defaultFilename;
-      if (settings.defaultFilename === "unix") defaultFilename = String(Date.now());
-      else if (settings.defaultFilename === "unknown") defaultFilename = "unknown";
-      else defaultFilename = generateFilename();
+    const settings = await browser.storage.local.get(["showFilenameBox", "clearOnPaste", "defaultFilename"]);
 
-      filenameInput.value = `${defaultFilename}.png`;
-      filenameInput.setAttribute("placeholder", `${defaultFilename}.png`);
-      filenameInput.setSelectionRange(0, defaultFilename.length);
+    const shadowStyleElement = document.createElement("style");
+    const iframeStyleElement = document.createElement("style");
 
-      if (!settings.showFilenameBox) filenameInput.style.display = "none";
+    const root = document.createElement("div");
 
-      aside.setAttribute("tabindex", -1);
+    shadowStyleElement.textContent = await shadowStyleRequest.text();
+    iframeStyleElement.textContent = await iframeStyleRequest.text();
+    iframe.srcdoc = await iframeRequest.text();
+    shadow.appendChild(shadowStyleElement);
+    shadow.append(root);
+    root.appendChild(iframe);
 
-      root.style.setProperty("--devicePixelRatio", window.devicePixelRatio);
+    const modalWidth = 250 / window.devicePixelRatio;
+    const modalHeight = 200 / window.devicePixelRatio;
 
-      const img = new Image();
-      img.src = URL.createObjectURL(clipboardImage);
-      await img.decode();
-      preview.style.backgroundImage = `url(${img.src})`;
-
-      // this sucks a LOT and is a terrible hack. At some point I should refactor the entire extension to use iframes.
-      for (const key in aside) {
-        if (/^on/.test(key)) {
-          const eventType = key.substring(2);
-          aside.addEventListener(eventType, (e) => e.stopPropagation());
-        }
-      }
-
-      aside.addEventListener(
-        "focusout",
-        (e) => {
-          aside.remove();
-          exportFunction(HTMLElement.prototype.blur, HTMLElement.prototype, { defineAs: "blur" });
-          exportFunction(HTMLElement.prototype.focus, HTMLElement.prototype, { defineAs: "focus" });
-        },
-        { once: true }
-      );
-
-      aside.addEventListener("keydown", (e) => {
-        if (e.key === "Escape") aside.dispatchEvent(new Event("focusout"));
-      });
-
-      filenameInput.addEventListener("keydown", (e) => {
-        if (e.key === "Enter") preview.dispatchEvent(new Event("click"));
-      });
-
-      preview.addEventListener(
-        "click",
-        async (event) => {
-          aside.dispatchEvent(new Event("focusout"));
-          if (settings.clearOnPaste) await navigator.clipboard.writeText("");
-
-          const dataTransfer = new DataTransfer();
-
-          let filename;
-          if (filenameInput.value === `${defaultFilename}.png` || filenameInput.value.length === 0) filename = `${defaultFilename}.png`;
-          else filename = filenameInput.value;
-
-          dataTransfer.items.add(new File([clipboardImage], filename, { type: "image/png" }));
-
-          e.target.files = dataTransfer.files;
-          e.target.dispatchEvent(new Event("input", { bubbles: true }));
-          e.target.dispatchEvent(new Event("change", { bubbles: true }));
-        },
-        { once: true }
-      );
-
-      selectAll.addEventListener(
-        "click",
-        () => {
-          aside.dispatchEvent(new Event("focusout"));
-          e.target.showPicker();
-        },
-        { once: true }
-      );
-
-      const modalWidth = 250 / window.devicePixelRatio;
-      const modalHeight = 200 / window.devicePixelRatio;
-
-      if (clientX + window.visualViewport.pageLeft < window.visualViewport.width + window.visualViewport.pageLeft - modalWidth) {
-        root.style.left = clientX + window.visualViewport.pageLeft + "px";
-      } else {
-        root.style.left = window.visualViewport.width + window.visualViewport.pageLeft - modalWidth + "px";
-      }
-
-      if (clientY + window.visualViewport.pageTop < window.visualViewport.height + window.visualViewport.pageTop - modalHeight) {
-        root.style.top = clientY + window.visualViewport.pageTop + "px";
-      } else {
-        root.style.top = clientY + window.visualViewport.pageTop - modalHeight + "px";
-      }
-
-      // temporarily stop the page from being able to change focus
-      // see https://github.com/vord1080/clipboard2file/issues/3#issuecomment-1024479980
-      exportFunction(() => {}, HTMLElement.prototype, { defineAs: "blur" });
-      exportFunction(() => {}, HTMLElement.prototype, { defineAs: "focus" });
-
-      aside.isPopup = true;
-      document.documentElement.appendChild(aside);
-      aside.focus({ preventScroll: true });
-
-      if (settings.showFilenameBox) filenameInput.focus();
+    if (clientX + window.visualViewport.pageLeft < window.visualViewport.width + window.visualViewport.pageLeft - modalWidth) {
+      root.style.left = clientX + window.visualViewport.pageLeft + "px";
     } else {
-      e.target.showPicker();
+      root.style.left = window.visualViewport.width + window.visualViewport.pageLeft - modalWidth + "px";
     }
+
+    if (clientY + window.visualViewport.pageTop < window.visualViewport.height + window.visualViewport.pageTop - modalHeight) {
+      root.style.top = clientY + window.visualViewport.pageTop + "px";
+    } else {
+      root.style.top = clientY + window.visualViewport.pageTop - modalHeight + "px";
+    }
+
+    document.documentElement.appendChild(aside);
+
+    await new Promise((resolve) => iframe.addEventListener("load", resolve, { once: true }));
+
+    iframe.contentDocument.body.appendChild(iframeStyleElement);
+
+    const preview = iframe.contentDocument.getElementById("preview");
+    const selectAll = iframe.contentDocument.getElementById("selectAll");
+    const filenameInput = iframe.contentDocument.getElementById("filename");
+
+    let defaultFilename;
+    if (settings.defaultFilename === "unix") defaultFilename = String(Date.now());
+    else if (settings.defaultFilename === "unknown") defaultFilename = "unknown";
+    else defaultFilename = generateFilename();
+
+    filenameInput.value = `${defaultFilename}.png`;
+    filenameInput.setAttribute("placeholder", `${defaultFilename}.png`);
+    filenameInput.setSelectionRange(0, defaultFilename.length);
+
+    if (!settings.showFilenameBox) filenameInput.style.display = "none";
+
+    const img = new Image();
+    img.src = URL.createObjectURL(clipboardImage);
+    await img.decode();
+    preview.style.backgroundImage = `url(${img.src})`;
+
+    root.style.setProperty("--devicePixelRatio", window.devicePixelRatio);
+    iframe.contentDocument.body.style.setProperty("--devicePixelRatio", window.devicePixelRatio);
+
+    iframe.contentDocument.addEventListener("keydown", (e) => {
+      if (e.key === "Escape") iframe.contentDocument.dispatchEvent(new Event("blur"));
+    });
+
+    filenameInput.addEventListener("keydown", (e) => {
+      if (e.key === "Enter") preview.dispatchEvent(new Event("click"));
+    });
+
+    preview.addEventListener(
+      "click",
+      async () => {
+        iframe.contentDocument.dispatchEvent(new Event("blur"));
+        if (settings.clearOnPaste) await navigator.clipboard.writeText("");
+
+        const dataTransfer = new DataTransfer();
+
+        let filename;
+        if (filenameInput.value === `${defaultFilename}.png` || filenameInput.value.length === 0) filename = `${defaultFilename}.png`;
+        else filename = filenameInput.value;
+
+        dataTransfer.items.add(new File([clipboardImage], filename, { type: "image/png" }));
+
+        e.target.files = dataTransfer.files;
+        e.target.dispatchEvent(new Event("input", { bubbles: true }));
+        e.target.dispatchEvent(new Event("change", { bubbles: true }));
+      },
+      { once: true }
+    );
+
+    selectAll.addEventListener(
+      "click",
+      () => {
+        iframe.contentDocument.dispatchEvent(new Event("blur"));
+        e.target.showPicker();
+      },
+      { once: true }
+    );
+
+    exportFunction(() => {}, HTMLElement.prototype, { defineAs: "blur" });
+    exportFunction(() => {}, HTMLElement.prototype, { defineAs: "focus" });
+
+    iframe.contentDocument.addEventListener("blur", () => {
+      aside.remove();
+      exportFunction(HTMLElement.prototype.blur, HTMLElement.prototype, { defineAs: "blur" });
+      exportFunction(HTMLElement.prototype.focus, HTMLElement.prototype, { defineAs: "focus" });
+    });
+
+    iframe.contentDocument.body.focus({ preventScroll: true });
+    if (settings.showFilenameBox) filenameInput.focus();
+
+    root.style.animationName = "finished";
   }
 }
 
@@ -144,29 +141,11 @@ document.addEventListener("pointerup", (e) => ((clientX = e.clientX), (clientY =
 // fix for extension not working on tinypng.com or any other website that stops propagation of input events. i hope this doesn't break anything.
 exportFunction(
   function () {
-    if (!this.target.isPopup) this.stopPropagation();
+    this.stopPropagation();
     if (this.type === "click") handleClick(this);
   },
   Event.prototype,
   { defineAs: "stopPropagation" }
-);
-
-// fix for extension not working on neo-lms.com
-exportFunction(
-  function () {
-    if (!this.target.isPopup) this.stopImmediatePropagation();
-  },
-  Event.prototype,
-  { defineAs: "stopImmediatePropagation" }
-);
-
-// fix for extension not working on neo-lms.com
-exportFunction(
-  function () {
-    if (!this.target.isPopup) this.preventDefault();
-  },
-  Event.prototype,
-  { defineAs: "preventDefault" }
 );
 
 function generateFilename() {
