1a453035356f090e9e5433da8e0dc2e4522cf585
Vord <32182314+vord1080@users.noreply.github.com>
2022-09-03 16:13:59 -0500

    Expand locale support to settings pane

--- settings/settings.js
+++ settings/settings.js
@@ -1,50 +1,43 @@
-if (!("randomUUID" in crypto))
-  // https://stackoverflow.com/a/2117523/2800218
-  // LICENSE: https://creativecommons.org/licenses/by-sa/4.0/legalcode
-  crypto.randomUUID = function randomUUID() {
-    return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) => (c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))).toString(16));
-  };
-
 window.addEventListener("DOMContentLoaded", async () => {
   const settings = {
     clearOnPaste: {
-      question: "Clear clipboard on paste?",
+      question: browser.i18n.getMessage("clearOnPaste"),
       answers: [
         {
-          diplayValue: "Yes",
+          diplayValue: browser.i18n.getMessage("true"),
           value: true,
         },
         {
-          diplayValue: "No",
+          diplayValue: browser.i18n.getMessage("false"),
           value: false,
           default: true,
         },
       ],
     },
     showFilenameBox: {
-      question: "Show text box for editing file name?",
+      question: browser.i18n.getMessage("showFilenameBox"),
       answers: [
         {
-          diplayValue: "Yes",
+          diplayValue: browser.i18n.getMessage("true"),
           value: true,
         },
         {
-          diplayValue: "No",
+          diplayValue: browser.i18n.getMessage("false"),
           value: false,
           default: true,
         },
       ],
     },
     defaultFilename: {
-      question: "What should the default file name be?",
+      question: browser.i18n.getMessage("defaultFilename"),
       answers: [
         {
-          diplayValue: "Formatted Time",
+          diplayValue: browser.i18n.getMessage("formattedTime"),
           value: "formatted",
           default: true,
         },
         {
-          diplayValue: "UNIX Timestamp",
+          diplayValue: browser.i18n.getMessage("unixTimestamp"),
           value: "unix",
         },
         {
